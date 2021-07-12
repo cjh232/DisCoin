9f3ff9da2fd6c54ae24facdf13e5dfa0e6af26be
cjh232 <46171942+cjh232@users.noreply.github.com>
2021-07-12 11:08:17 -0400

    refactor(wallets): Added proper documentation

--- utils/factories.py
+++ utils/factories.py
@@ -6,8 +6,9 @@ import ecdsa
 
 NO_PARAMS = "Wallet factory requires either a name or a wallet dictionary."
 
+
 class WalletBuildError(Exception):
-    
+
     def __init__(self, message="Error building wallet"):
         self.message = message
         super().__init__(self.message)
@@ -16,8 +17,8 @@ class WalletBuildError(Exception):
 class WalletFactory:
 
     STARTING_AMOUNT = 500
-    
-    def create_wallet(self, name = None, wallet_dict = None):
+
+    def create_wallet(self, name=None, wallet_dict=None):
 
         if not wallet_dict and not name:
             raise WalletBuildError(NO_PARAMS)
@@ -27,18 +28,16 @@ class WalletFactory:
 
         return wallets.Wallet(wallet_dict)
 
-
     # Generates ECDSA key pair
+
     def generate_key_pair(self):
         sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
         private_key = sk.to_string().hex()
 
         vk = sk.get_verifying_key()
-        public_key = vk.to_string().hex()
+        public_key = vk.to_string()  # Bytes
 
-        print('Public Key:', public_key)
-        
-        public_key = base64.b64encode(bytes.fromhex(public_key))
+        public_key = base64.b64encode(public_key)
 
         return [private_key, public_key]
 
@@ -47,18 +46,23 @@ class WalletFactory:
         [private_key, public_key] = self.generate_key_pair()
 
         return {
-            'owner': name, 
-            'address': public_key,
+            'owner': name,
+            'address': hashlib.sha256(base64.b64decode(public_key)).hexdigest(),
             'private_key': private_key,
+            'public_key': public_key
         }
 
+
 factory = WalletFactory()
+
+public_key = b'DPcRVZy+FCHJzdTRQb42OZ/HLH+jkdSVLestfSKu5W+p43N+Fy9mR4Y3RjcHhpGrtIvYQ4O/Pu6eBPMfXFzCag=='
 
 
 existing_wallet = {
     'owner': 'Chris',
-    'address': b'DPcRVZy+FCHJzdTRQb42OZ/HLH+jkdSVLestfSKu5W+p43N+Fy9mR4Y3RjcHhpGrtIvYQ4O/Pu6eBPMfXFzCag==',
-    'private_key': '90fbae98d03107a1f3c13de6fbab137c455a7911d7607426d5483624dff662c2'
+    'address': hashlib.sha256(base64.b64decode(public_key)).hexdigest(),
+    'private_key': '90fbae98d03107a1f3c13de6fbab137c455a7911d7607426d5483624dff662c2',
+    'public_key': b'DPcRVZy+FCHJzdTRQb42OZ/HLH+jkdSVLestfSKu5W+p43N+Fy9mR4Y3RjcHhpGrtIvYQ4O/Pu6eBPMfXFzCag=='
 }
 
 w2 = factory.create_wallet(name="Chris")
@@ -68,4 +72,3 @@ w2.print_wallet()
 tx = w2.create_transaction('Dan', 500)
 
 print(w2.verify_transaction(tx))
-
--- utils/wallets.py
+++ utils/wallets.py
@@ -1,75 +1,150 @@
+"""
+Contains the definition of the Wallet class.
+
+"""
+
+
 import hashlib
-import ecdsa
-import json
 import base64
+import json
+import ecdsa
+
+
+def encode_transaction(transaction):
+    """Returns encoded transaction"""
+    return json.dumps(transaction, sort_keys=True).encode()
 
 
 class Wallet:
 
+    """
+    A class to represent a wallet.
+
+    ...
+    Attributes
+    ----------
+    owner : str
+        name of the wallet owner
+    address : str
+        256-bit hashed address of the wallet owner.
+        Also links to the owners public key stored in the db
+    private_key : (temporary)
+        wallet ECDSA private key used to sign new transactions.
+        IMPORTANT: This is temporary and will be removed in the
+        future when this project is setup with Discord. The user will
+        be responsible for keeping track of his/her private key.
+
+        Converted to hex, must be converted back to bytes before
+        being used
+    public_key :
+        wallets ECDSA public key. Encoded in base64, needs to be
+        decoded before being used.
+    """
+
     def __init__(self, wallet_dict):
         self.owner = wallet_dict["owner"]
         self.address = wallet_dict["address"]
+        self.public_key = wallet_dict["public_key"]
         self.private_key = wallet_dict["private_key"]
 
-    
     @property
     def amount(self):
-        # This function will need to sum all the utxo linked to this wallets address.
-        return 5  
+        """Return a sum of all the UTXO linked to this wallet's address"""
+        return 5
 
     def create_transaction(self, out_addr, amount):
+        """Form a valid transaction.
 
-        tx = {
-            'sender': self.address.decode(),
+        Arguments:
+        ----------
+        out_addr -- the address of the wallet receiving the output
+        amount -- total of the transaction
+
+        Combine an array of UTXO that are >= to the amount specified (input).
+        Create a resulting array, containing at least one UTXO, to the receiving
+        wallet (output). If the input sum > amount, a second UTXO will be created to store
+        the 'change'. This UTXO is addressed to the current wallet address.
+        """
+        new_tx = {
+            'sender': self.address,
             'receiver': out_addr,
             'amount': amount
         }
 
-        tx['sig'] = self.sign_transaction(tx)
+        new_tx['sig'] = self.sign_transaction(new_tx)
 
-        return tx
+        return new_tx
 
-    def sign_transaction(self, tx):
+    def sign_transaction(self, new_tx):
+        """Creates and returns a transaction signature.
 
-        enc_tx = self.encode(tx)
+        Arguments:
+        ----------
+        new_tx -- New transaction to be signed
+        private_key -- Private key associated with this wallet.
+
+         """
+
+        enc_tx = encode_transaction(new_tx)
 
         # When the key pair is created we convert the private key to hex. from_string requires
         # a byte string so we need to make the conversion. Otherwise we will get a malformed
         # key error thrown.
 
-        sk = ecdsa.SigningKey.from_string(bytes.fromhex(self.private_key), curve=ecdsa.SECP256k1)
+        signing_key = ecdsa.SigningKey.from_string(bytes.fromhex(
+            self.private_key), curve=ecdsa.SECP256k1)
 
-        signature = base64.b64encode(sk.sign(enc_tx, hashfunc=hashlib.sha256))
+        signature = base64.b64encode(
+            signing_key.sign(enc_tx, hashfunc=hashlib.sha256))
 
         return signature
-        
-    def verify_transaction(self, tx):
+
+    def verify_transaction(self, unverified_tx):
+        """Verifies transaction signature
+
+        IMPORTANT: This function may be better fit as a method of blockchain
+        which will verify the transaction before adding it to the transaction
+        pool
+
+        Arguments:
+        ----------
+        unverified_tx -- Transaction to be verified
+        public_key -- Public key of the wallet suspected of making this transaction
+
+
+        """
         # When the key pair is created we encode the public key in base64 to create a shorter
         # key. Before reconstructing, using from_string(), we must decode it. Otherwise we will get
         # a malformed key error thrown.
 
-        vk = ecdsa.VerifyingKey.from_string(base64.b64decode(self.address), curve=ecdsa.SECP256k1)
+        verifying_key = ecdsa.VerifyingKey.from_string(
+            base64.b64decode(self.public_key), curve=ecdsa.SECP256k1)
 
-        tx_headers = {'sender': tx['sender'], 'receiver': tx['receiver'], 'amount': tx['amount']}
+        tx_headers = {
+            'sender': unverified_tx['sender'],
+            'receiver': unverified_tx['receiver'],
+            'amount': unverified_tx['amount']
+        }
 
-        is_valid = vk.verify(base64.b64decode(tx['sig']), self.encode(tx_headers), hashfunc=hashlib.sha256)
-        
+        is_valid = verifying_key.verify(base64.b64decode(unverified_tx['sig']), encode_transaction(
+            tx_headers), hashfunc=hashlib.sha256)
+
         return is_valid
 
-
-    def encode(self, tx):
-        return json.dumps(tx, sort_keys=True).encode()
-        
-
     def serialize(self):
+        """Converts the wallet to a JSON object"""
         return {
             'owner': self.owner,
             'address': self.address,
+            'public_key': self.public_key,
             'private_key': self.private_key
         }
-    
-    def print_wallet(self):
-        print(f'Owner: {self.owner}\nAddress: {self.address}\nPrivate Key: {self.private_key}')
-        
 
-    
+    def print_wallet(self):
+        """Prints the wallet to the console"""
+        print(
+            f'Owner: {self.owner}\n'
+            f'Address: {self.address}\n'
+            f'Private Key: {self.private_key}\n'
+            f'Public Key: {self.public_key}\n'
+        )
